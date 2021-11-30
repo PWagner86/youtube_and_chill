5583658f87e243ce2ad7e2e9eeb60c358fa8af9f
Peter Wagner <peter.wagner1986@gmail.com>
2021-11-30 18:53:20 +0100

    tried to connect. not working

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import socketIOClient from 'socket.io-client';
-// https://www.valentinog.com/blog/socket-react/
 import { Header } from './components/Header/Header';
 import { PlayerContainer } from './components/PlayerContainer/PlayerContainer';
 import './index.css';
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <PlayerContainer/>
-      {/* <form action="../../post" method="post">
-        <button type="submit">Connected?</button>
-      </form> */}
     </div>
   );
 }
--- src/components/Controles/Controles.tsx
+++ src/components/Controles/Controles.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { io, Socket } from 'socket.io-client';
+
 
 export const Controles = () => {
+
+  const [socket, setSocket]: any = useState();
+
+
+  useEffect(() => {
+    const s: any = io('http://localhost:3001');
+    setSocket(s);
+
+    return () => {
+      s.disconnect();
+    }
+  }, []) 
+
+
+
     return (
         <div className="controles">
-            <button className="play">Play</button>
+            <button 
+                className="play"
+                onClick={ clickPlay(socket) }
+            >Play</button>
             
         </div>
     )
 };
+
+const clickPlay = (socket: any): any => {
+    socket.on('click-event', () => {
+        console.log('Play-Button clicked');
+    })
+}
